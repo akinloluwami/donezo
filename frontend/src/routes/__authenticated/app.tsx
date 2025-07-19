38b59969c74584a41577d9240f42123f2bb5f598
titanium-dev <akinkunmioye42@gmail.com>
2025-07-19 14:16:30 +0100

    Implement label and task loading in RouteComponent using Zustand store

--- frontend/src/routes/__authenticated/app.tsx
+++ frontend/src/routes/__authenticated/app.tsx
@@ -1,11 +1,20 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 import Sidebar from "../../components/sidebar";
+import { useEffect } from "react";
+import { useLabelsStore } from "../../lib/labels-store";
+import { useTasksStore } from "../../lib/tasks-store";
 
 export const Route = createFileRoute("/__authenticated/app")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const loadLabels = useLabelsStore((s) => s.loadLabels);
+  const loadTasks = useTasksStore((s) => s.loadTasks);
+  useEffect(() => {
+    loadLabels();
+    loadTasks();
+  }, [loadLabels, loadTasks]);
   return (
     <div className="flex h-screen bg-gray-50 relative">
       <div className="bg-[#7eb3f4]/50 h-[900px] w-full opacity-50 rounded-full absolute -top-[800px] left-1/2 -translate-x-1/2 blur-3xl z-0 pointer-events-none"></div>
