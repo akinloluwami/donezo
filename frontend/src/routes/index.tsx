88157c017c85cd9c621e88fd5a28d88ca1606a7e
titanium-dev <akinkunmioye42@gmail.com>
2025-07-19 08:14:44 +0100

    Update RouteComponent styles and links for improved layout and navigation

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -8,7 +8,7 @@ export const Route = createFileRoute("/")({
 
 function RouteComponent() {
   return (
-    <div className="relative z-0">
+    <div className="relative z-0 px-5 lg:px-10">
       <div className="bg-[#7eb3f4]/50 size-[900px] rounded-full absolute -top-[800px] left-1/2 -translate-x-1/2 blur-3xl z-0 pointer-events-none"></div>
       <div className="relative w-full max-w-4xl h-20 rounded-full border border-gray-400/20 mx-auto z-10 mt-5 bg-white/10 backdrop-blur-lg p-5 flex items-center justify-between">
         <h1 className="font-medium text-lg">Donezo</h1>
@@ -17,7 +17,7 @@ function RouteComponent() {
             Login
           </Link>
           <Link
-            to="/"
+            to="/signup"
             className="bg-accent rounded-full text-white px-5 py-3 hover:bg-accent/90 transition-colors"
           >
             Sign up
@@ -26,19 +26,19 @@ function RouteComponent() {
       </div>
 
       <div className="flex w-full max-w-4xl mx-auto lex-col mt-10 flex-col">
-        <h1 className="text-8xl mt-14 font-bold text-gray-800">
+        <h1 className="lg:text-8xl text-5xl mt-14 font-bold text-gray-800">
           A more humane
           <br />
           <span className="bg-gradient-to-r from-accent/70 to-gray-300 from-40% bg-clip-text text-transparent">
             task manager
           </span>
         </h1>
-        <p className="text-gray-600 text-xl mt-5 max-w-3xl">
+        <p className="text-gray-600 lg:text-xl mt-5 max-w-3xl">
           Designed to reduce overwhelm, Donezo makes it easy to plan, track, and
           complete tasks—your way.
         </p>
 
-        <Link to="/" className="w-fit mt-10">
+        <Link to="/signup" className="w-fit mt-10">
           <Button className="w-full">
             Get Started
             <MoveRight />
