0a79c3392f2abc84eb794adacceaeed780d13f9a
titanium-dev <akinkunmioye42@gmail.com>
2025-07-19 08:46:18 +0100

    Update RouteComponent to conditionally render links based on user authentication status

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,27 +1,42 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { MoveRight } from "lucide-react";
 import { Button } from "../components/button";
+import { useUserStore } from "../lib/user-store";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const user = useUserStore((s) => s.user);
+
   return (
     <div className="relative z-0 px-5 lg:px-10">
       <div className="bg-[#7eb3f4]/50 size-[900px] rounded-full absolute -top-[800px] left-1/2 -translate-x-1/2 blur-3xl z-0 pointer-events-none"></div>
       <div className="relative w-full max-w-4xl h-20 rounded-full border border-gray-400/20 mx-auto z-10 mt-5 bg-white/10 backdrop-blur-lg p-5 flex items-center justify-between">
         <h1 className="font-medium text-lg">Donezo</h1>
         <div className="flex items-center gap-5">
-          <Link to="/login" className="text-gray-500">
-            Login
-          </Link>
-          <Link
-            to="/signup"
-            className="bg-accent rounded-full text-white px-5 py-3 hover:bg-accent/90 transition-colors"
-          >
-            Sign up
-          </Link>
+          {!user && (
+            <Link to="/login" className="text-gray-500">
+              Login
+            </Link>
+          )}
+
+          {user ? (
+            <Link
+              to="/app/home"
+              className="bg-accent rounded-full text-white px-5 py-3 hover:bg-accent/90 transition-colors"
+            >
+              Go to app
+            </Link>
+          ) : (
+            <Link
+              to="/signup"
+              className="bg-accent rounded-full text-white px-5 py-3 hover:bg-accent/90 transition-colors"
+            >
+              Sign up
+            </Link>
+          )}
         </div>
       </div>
 
@@ -38,12 +53,21 @@ function RouteComponent() {
           complete tasks—your way.
         </p>
 
-        <Link to="/signup" className="w-fit mt-10">
-          <Button className="w-full">
-            Get Started
-            <MoveRight />
-          </Button>
-        </Link>
+        {user ? (
+          <Link to="/app/home" className="w-fit mt-10">
+            <Button className="w-full">
+              Go to app
+              <MoveRight />
+            </Button>
+          </Link>
+        ) : (
+          <Link to="/signup" className="w-fit mt-10">
+            <Button className="w-full">
+              Get Started
+              <MoveRight />
+            </Button>
+          </Link>
+        )}
       </div>
     </div>
   );
