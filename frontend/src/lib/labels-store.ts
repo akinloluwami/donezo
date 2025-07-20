c2f8485ecaf921a2927c2e76d35e6e28b29751fc
titanium-dev <akinkunmioye42@gmail.com>
2025-07-20 10:55:05 +0100

    Integrate labels management with indexedDB, enabling CRUD operations for labels in the store

--- frontend/src/lib/labels-store.ts
+++ frontend/src/lib/labels-store.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { appClient } from "./app-client";
+import { labelsDB } from "./indexed-db";
 
 export type Label = {
   id: string;
@@ -16,18 +17,32 @@ type LabelsStore = {
   loadLabels: () => Promise<void>;
 };
 
-export const useLabelsStore = create<LabelsStore>((set) => ({
+export const useLabelsStore = create<LabelsStore>((set, get) => ({
   labels: [],
-  setLabels: (labels) => set({ labels }),
-  addLabel: (label) => set((state) => ({ labels: [...state.labels, label] })),
-  updateLabel: (id, data) =>
+  setLabels: (labels) => {
+    set({ labels });
+    labels.forEach((label) => labelsDB.add(label));
+  },
+  addLabel: (label) => {
+    set((state) => ({ labels: [...state.labels, label] }));
+    labelsDB.add(label);
+  },
+  updateLabel: (id, data) => {
     set((state) => ({
       labels: state.labels.map((l) => (l.id === id ? { ...l, ...data } : l)),
-    })),
-  removeLabel: (id) =>
-    set((state) => ({ labels: state.labels.filter((l) => l.id !== id) })),
+    }));
+    const updatedLabel = get().labels.find((l) => l.id === id);
+    if (updatedLabel) labelsDB.add(updatedLabel);
+  },
+  removeLabel: (id) => {
+    set((state) => ({ labels: state.labels.filter((l) => l.id !== id) }));
+    labelsDB.delete(id);
+  },
   loadLabels: async () => {
     const labels = await appClient.labels.getLabels();
     set({ labels });
+    for (const label of labels) {
+      await labelsDB.add(label);
+    }
   },
 }));
