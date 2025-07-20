1a376f0ce0a58ccd486fc6eafaaf262cd4947396
titanium-dev <akinkunmioye42@gmail.com>
2025-07-20 09:39:29 +0100

    Integrate user loading functionality in RootComponent to enhance user session management

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,12 +1,19 @@
 import * as React from "react";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { Toaster } from "sonner";
+import { useUserStore } from "../lib/user-store";
 
 export const Route = createRootRoute({
   component: RootComponent,
 });
 
 function RootComponent() {
+  const loadUser = useUserStore((s) => s.fetchUser);
+
+  React.useEffect(() => {
+    loadUser();
+  }, [loadUser]);
+
   return (
     <React.Fragment>
       <Toaster richColors />
