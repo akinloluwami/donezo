3da94fcaf6000c1d6bc639226d71b99d8fcebe79
titanium-dev <akinkunmioye42@gmail.com>
2025-07-20 09:11:39 +0100

    Add Toaster component to RootComponent for enhanced notification support

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { Toaster } from "sonner";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -8,6 +9,7 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <React.Fragment>
+      <Toaster richColors />
       <Outlet />
     </React.Fragment>
   );
