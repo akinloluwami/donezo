89d53e516a7e4903b34d2633dec6cc0c28fed11c
titanium-dev <akinkunmioye42@gmail.com>
2025-07-20 09:11:47 +0100

    Integrate collections store into RouteComponent to load collections on mount

--- frontend/src/routes/__authenticated/app.tsx
+++ frontend/src/routes/__authenticated/app.tsx
@@ -5,6 +5,7 @@ import { useLabelsStore } from "../../lib/labels-store";
 import { useTasksStore } from "../../lib/tasks-store";
 import TaskSideSheet from "../../components/task-side-sheet";
 import { AnimatePresence } from "framer-motion";
+import { useCollectionsStore } from "../../lib/collections-store";
 
 export const Route = createFileRoute("/__authenticated/app")({
   component: RouteComponent,
@@ -13,16 +14,18 @@ export const Route = createFileRoute("/__authenticated/app")({
 function RouteComponent() {
   const loadLabels = useLabelsStore((s) => s.loadLabels);
   const loadTasks = useTasksStore((s) => s.loadTasks);
+  const loadCollections = useCollectionsStore((s) => s.loadCollections);
   const tasks = useTasksStore((s) => s.tasks);
   const selectedTaskId = useTasksStore((s) => s.selectedTaskId);
   const setSelectedTaskId = useTasksStore((s) => s.setSelectedTaskId);
-
   const selectedTask = tasks.find((t) => t.id === selectedTaskId) || null;
 
   useEffect(() => {
     loadLabels();
     loadTasks();
-  }, [loadLabels, loadTasks]);
+    loadCollections();
+  }, [loadLabels, loadTasks, loadCollections]);
+
   return (
     <div className="flex h-screen bg-[#eceef0] relative">
       <div className="bg-[#7eb3f4]/50 h-[900px] w-full opacity-50 rounded-full absolute -top-[800px] left-1/2 -translate-x-1/2 blur-3xl z-0 pointer-events-none"></div>
